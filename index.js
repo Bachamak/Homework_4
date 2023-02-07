cd354bced05ee08395732b1b17ac30716d946389
Bachamak <tarasenkodaniilromanovich@gmail.com>
2023-02-07 23:35:07 +0200

    Измененный код

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 let threeNumbers = prompt('Write three numbers using commas between them');
 
-let firstNumber = threeNumbers[0];
-let middleNumber = threeNumbers[1];
-let lastNumber = threeNumbers[threeNumbers.length -1];
+let massiveOfNumber = threeNumbers.split(',');
+
+let firstNumber = massiveOfNumber[0];
+let middleNumber = massiveOfNumber[1];
+let lastNumber = massiveOfNumber[massiveOfNumber.length -1];
 
 let sumNumbers = +firstNumber + +middleNumber + +lastNumber;
 
 let min;
 let max;
 
-if (threeNumbers[0] > threeNumbers[1] && threeNumbers[0] > threeNumbers[2]) {
-    max = threeNumbers[0];
-} else if (threeNumbers[1] > threeNumbers[0] && threeNumbers[1] > threeNumbers[2]) {
-    max = threeNumbers[1];
-} else (threeNumbers[2] > threeNumbers[0] && threeNumbers[2] > threeNumbers[1]); {
-    max = threeNumbers[2];
+if (massiveOfNumber[0] > massiveOfNumber[1] && massiveOfNumber[0] > massiveOfNumber[2]) {
+    max = massiveOfNumber[0];
+} else if (massiveOfNumber[1] > massiveOfNumber[0] && massiveOfNumber[1] > massiveOfNumber[2]) {
+    max = massiveOfNumber[1];
+} else (massiveOfNumber[2] > massiveOfNumber[0] && massiveOfNumber[2] > massiveOfNumber[1]); {
+    max = massiveOfNumber[2];
 }
 
-if (threeNumbers[0] < threeNumbers[1] && threeNumbers[0] < threeNumbers[2]) {
-    min = threeNumbers[0];
-} else if (threeNumbers[1] < threeNumbers[0] && threeNumbers[1] < threeNumbers[2]) {
-    min = threeNumbers[1];
+if (massiveOfNumber[0] < massiveOfNumber[1] && massiveOfNumber[0] < massiveOfNumber[2]) {
+    min = massiveOfNumber[0];
+} else if (massiveOfNumber[1] < massiveOfNumber[0] && massiveOfNumber[1] < massiveOfNumber[2]) {
+    min = massiveOfNumber[1];
+} else (massiveOfNumber[2] < massiveOfNumber[0] && massiveOfNumber[2] < massiveOfNumber[1]); {
+    min = massiveOfNumber[2];
 }
 
 alert(`first number is ${firstNumber}, last number is ${lastNumber}.`);
@@ -28,8 +32,8 @@ alert(`Пользователь ввел ${firstNumber} + ${middleNumber} + ${la
 alert(`min is ${min}, max is ${max}`);
 
 let askQuestion = prompt("Please enter one of the numbers which you entered before?");
-if(threeNumbers.indexOf(askQuestion) === -1) {
+if(massiveOfNumber.indexOf(askQuestion) === -1) {
     alert("Sorry, you did not enter this number!");
 } else {
-    alert(threeNumbers.indexOf(askQuestion));
+    alert(massiveOfNumber.indexOf(askQuestion));
 }
